7a09d08688f32465ef92c499d68821c0079543a7
yehongjiang <yehongjiang2012@gmail.com>
2018-05-05 20:13:12 +0800

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,24 @@
-from flask import Flask
+import hashlib
+
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, I love Digital Ocean!"
+@app.route('/', methods=['GET', 'POST'])
+def wechat_auth():
+    if request.method == 'GET':
+        token = 'hello_ian'  # your token
+        query = request.args  # GET 方法附上的参数
+        signature = query.get('signature', '')
+        timestamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
+        s = [timestamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
+            return make_response(echostr)
 
 
 if __name__ == "__main__":
