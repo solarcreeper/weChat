173bcb05e7857d19ec2e4c53fa9cb5c7e482adb6
yehongjiang <yehongjiang2012@gmail.com>
2018-05-06 00:25:04 +0800

    .

--- app/__init__.py
+++ app/__init__.py
@@ -21,20 +21,21 @@ def wechat_auth():
         s = ''.join(s)
         if (hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
-    print(request.data)
-    data = request.data
-    if type(data) == bytes:
-        data = data.decode('utf-8')
-    xml_recived = ET.fromstring(data)
+    else:
+        print(request.data)
+        data = request.data
+        if type(data) == bytes:
+            data = data.decode('utf-8')
+        xml_recived = ET.fromstring(data)
 
-    to_username = xml_recived.find("ToUserName").text
-    from_username = xml_recived.find("FromUserName").text
-    content = xml_recived.find("Content").text
-    reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
-    reply = reply % (from_username, to_username, str(int(time.time())), content)
-    response = make_response(reply)
-    response.content_type = 'application/xml'
-    return response
+        to_username = xml_recived.find("ToUserName").text
+        from_username = xml_recived.find("FromUserName").text
+        content = xml_recived.find("Content").text
+        reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        reply = reply % (from_username, to_username, str(int(time.time())), content)
+        response = make_response(reply)
+        response.content_type = 'application/xml'
+        return response
 
 
 if __name__ == "__main__":
