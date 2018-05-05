1bc71cf940fe8f77a9bc09095a1ff99a96f0823f
yehongjiang <yehongjiang2012@gmail.com>
2018-05-05 20:33:58 +0800

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 import hashlib
+import time
+import xml.etree.ElementTree as ET
 
 from flask import Flask, request, make_response
 
@@ -19,6 +21,14 @@ def wechat_auth():
         s = ''.join(s)
         if (hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
+    xml_recived = ET.fromstring(request.data)
+    to_username = xml_recived.find("ToUserName").text
+    from_username = xml_recived.find("FromUserName").text
+    content = xml_recived.find("Content").text
+    reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+    response = make_response(reply % (from_username, to_username, str(int(time.time())), content))
+    response.content_type = 'application/xml'
+    return response
 
 
 if __name__ == "__main__":
