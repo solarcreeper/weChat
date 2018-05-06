dd5ce3254f073d2597ecdb984f5ecbf718d36097
yehongjiang <yehongjiang2012@gmail.com>
2018-05-07 00:25:33 +0800

    .

--- app/models.py
+++ app/models.py
@@ -6,14 +6,14 @@ import time
 def content_parse(collection, content, from_user):
     parser = content.split(' ')
     if parser[0] != 'record' or len(parser) < 2 or len(parser) % 2 != 1:
-        content = "暂不支持此操作"
+        content = u"暂不支持此操作"
     else:
         i = 1
         data = {}
         while(i < len(parser)):
             data[parser[i]] = parser[i+1]
             i = i + 2
-        content = "记录完成"
+        content = u"记录完成"
         record_templete = {'user': from_user, 'timestamp':  time.strftime("%Y-%m-%d", time.localtime())}
         record = collection.find_one(record_templete)
         if record is None:
@@ -25,4 +25,4 @@ def content_parse(collection, content, from_user):
         if 'situp' in parser:
             record['situp'] = data['situp']
         obj_id = collection.save(record)
-    return content.encode('utf-8')
+    return content
