12e01ba6e1996f86b00083e3f3bb2165eececbdc
yehongjiang <yehongjiang2012@gmail.com>
2018-05-08 23:07:31 +0800

    .

--- app/models.py
+++ app/models.py
@@ -4,68 +4,72 @@ import time
 
 
 def content_parse(collection, content, from_user):
-    ckeck, ckeck_reply, parsed_record, parsed_not_allowd_record = check_content(content)
+    ckeck, parser_result = check_content(content)
     if not ckeck:
-        content = ckeck_reply
-        return content
+        return parser_result
 
     record_templete = {'user': from_user, 'timestamp': time.strftime("%Y-%m-%d", time.localtime())}
-    if len(parsed_record) == 1:
-        content = run_qr(collection, parsed_record, record_templete)
+    if parser_result[0] in ['q', 'r']:
+        content = run_qr(collection, parser_result, record_templete)
     else:
-        content = run_upadte(collection, parsed_record, record_templete)
-
-    if len(parsed_not_allowd_record) > 0:
-        content = content + u'以下数据格式不正确【%s】' % str(parsed_not_allowd_record)
+        content = run_upadte(collection, parser_result, record_templete)
     return content
 
-def run_qr(collection, parser,record_templete):
+
+def run_qr(collection, parser, record_templete):
     record = collection.find_one(record_templete)
     if parser[0] == 'r':
         collection.remove(record)
-        content = u'删除记录【%s】' %record
+        content = u'删除记录【%s】' % record
     else:
-        content = u'查询结果【%s】' %record
+        content = u'查询结果【%s】' % record
     return content
+
 
 def run_upadte(collection, parser, record_templete):
     record = collection.find_one(record_templete)
-    record.update(parser)
-    return u'更新记录【%s】' %record
+    if record is None:
+        record = record_templete
+    record.update(list2dict(parser))
+    collection.save(record)
+    return u'更新记录【%s】' % record
+
 
 def check_content(content):
+    allowed_record = ['bike', 'flat', 'situp']
     parser = content.split(' ')
-    ckeck_reply = None
-    if parser[0].lower() != 'record':
-        ckeck_reply = u'使用record bike|situp|flat num 格式进行记录'
-        ckeck = False
-    elif len(parser) == 2 and (parser[1] != 'q' or parser[1] != 'r'):
-        ckeck_reply = u'格式不正确，目前只支持{record q|r}and {record bike|situp|flat num}操作'
-        ckeck = False
-    elif len(parser) < 2:
-        ckeck_reply = u'格式不正确，目前只支持record q|r 操作'
-        ckeck = False
-    elif len(parser) > 2 and len(parser) % 2 == 0:
-        ckeck_reply = u'格式不正确，目前只支持{record q|r}and {record bike|situp|flat num}操作'
-        ckeck = False
-    else:
-        ckeck = True
+    if parser[0].lower() != 'record' or len(parser) < 2:
+        return False, u'使用{record bike|situp|flat num} or {record q|r}格式进行记录'
 
-    if ckeck:
-        allowed_record = ['bike', 'flat', 'situp']
-        record = {}
-        not_allowed_record = {}
-        i = 1
-        while (i < len(parser)):
-            if parser[i] in allowed_record:
-                record[parser[i]] = parser[i + 1]
-            else:
-                not_allowed_record[parser[i]] = parser[i+1]
-            i = i + 2
-        if len(record) == 0:
-            ckeck_reply = u'使用record bike|situp|flat num 格式进行记录'
+    ckeck = True
+    parser_result = None
+    if len(parser) == 2:
+        if parser[1] in ['q', 'r']:
+            parser_result = parser[1:]
+        else:
+            parser_result = u'使用{record bike|situp|flat num} or {record q|r}格式进行记录'
             ckeck = False
     else:
-        record = None
-        not_allowed_record = None
-    return ckeck, ckeck_reply, record, not_allowed_record
+        if len(parser) % 2 == 1:
+            i = 1
+            parser_result = parser[1:]
+            while i < len(parser):
+                if parser[i] not in allowed_record:
+                    parser_result = u'使用{record bike|situp|flat num} or {record q|r}格式进行记录'
+                    ckeck = False
+                    break
+                else:
+                    i = i + 2
+        else:
+            parser_result = u'使用{record bike|situp|flat num} or {record q|r}格式进行记录'
+            ckeck = False
+    return ckeck, parser_result
+
+
+def list2dict(l):
+    i = 0
+    data = {}
+    while i < len(l):
+        data[l[i]] = l[i + 1]
+        i = i + 2
+    return data
