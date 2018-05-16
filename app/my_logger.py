e2e080c0ef850dd0b6997ac3e0b82e9b3976a70a
yehongjiang <yehongjiang2012@gmail.com>
2018-05-16 15:55:50 +0800

    .

--- app/my_logger.py
+++ app/my_logger.py
@@ -4,12 +4,12 @@ import os
 from time import strftime
 import logging
 
-FLASK_LOG_DIR = os.path.join(os.getenv("HOME"), 'log/flask/log_{}'.format(strftime('%Y%m%d')))
-
-if os.path.exists(os.path.dirname(FLASK_LOG_DIR)):
-    os.makedirs(os.path.dirname(FLASK_LOG_DIR))
-
 def init_logger(level, log_name=None):
+    FLASK_LOG_DIR = os.path.join(os.getenv("HOME"), 'log/flask/log_{}'.format(strftime('%Y%m%d')))
+
+    if os.path.exists(os.path.dirname(FLASK_LOG_DIR)):
+        os.makedirs(os.path.dirname(FLASK_LOG_DIR))
+
     logger = logging.getLogger(log_name)
     logger.setLevel(level)
 
