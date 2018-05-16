c613780d7b0acd0bbfb6f3ba0b5a8af143338692
yehongjiang <yehongjiang2012@gmail.com>
2018-05-16 21:37:28 +0800

    .

--- app/my_logger.py
+++ app/my_logger.py
@@ -3,16 +3,22 @@
 import os, stat
 from time import strftime
 import logging
+import platform
 
 
 def init_logger(level, log_name=None):
-    if (os.getenv("HOME") is None):
+    # if (os.getenv("HOME") is None):
+    #     FLASK_LOG_DIR = os.path.join(os.path.abspath('.'), 'log')
+    # else:
+    #     FLASK_LOG_DIR = os.path.join(os.getenv('HOME'), 'log')
+    #
+    # if not os.path.exists(FLASK_LOG_DIR):
+    #     os.makedirs(FLASK_LOG_DIR)
+
+    if platform.platform().startswith("Windows"):
         FLASK_LOG_DIR = os.path.join(os.path.abspath('.'), 'log')
     else:
-        FLASK_LOG_DIR = os.path.join(os.getenv('HOME'), 'log')
-
-    if not os.path.exists(FLASK_LOG_DIR):
-        os.makedirs(FLASK_LOG_DIR)
+        FLASK_LOG_DIR = '/home/log'
 
     FLASK_LOG_FILE = os.path.join(FLASK_LOG_DIR, 'log_{}.log'.format(strftime('%Y%m%d_%H%M%S')))
 
