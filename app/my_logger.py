2fc00b42de5d2c27575bd8981265cba474c25d5d
yehongjiang <yehongjiang2012@gmail.com>
2019-02-24 20:46:38 +0800

    .

--- app/my_logger.py
+++ app/my_logger.py
@@ -10,7 +10,7 @@ def init_logger(level, log_name=None):
     if platform.platform().startswith("Windows"):
         FLASK_LOG_DIR = os.path.join(os.path.abspath('.'), 'log')
     else:
-        FLASK_LOG_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'log')
+        FLASK_LOG_DIR = os.path.join(os.path.abspath(os.path.dirname(__name__)), 'log')
 
     if not os.path.exists(FLASK_LOG_DIR):
         os.makedirs(FLASK_LOG_DIR)
